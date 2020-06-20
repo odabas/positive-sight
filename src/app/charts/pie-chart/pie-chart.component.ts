b07bd3e93189e7cbfc36a07c5c740a19d34bfb21
Onur Odabas <tr.tc@msn.com>
2020-06-20 17:35:23 +0300

    Pie and Doughnut chart adde

--- src/app/charts/pie-chart/pie-chart.component.ts
+++ src/app/charts/pie-chart/pie-chart.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pie-chart',
-  templateUrl: './pie-chart.component.html',
-  styleUrls: ['./pie-chart.component.css']
+  selector: "app-pie-chart",
+  templateUrl: "./pie-chart.component.html",
+  styleUrls: ["./pie-chart.component.css"],
 })
 export class PieChartComponent implements OnInit {
+  constructor() {}
+  pieChartData: number[] = [350, 450, 120];
+  pieChartLabels: string[] = [
+    "XYZ Logistics",
+    "Main St Bakery",
+    "Acme Hosting",
+  ];
+  colors: any[] = [
+    {
+      backgroundColor: ["#26547c", "#ff6b6b", "#ffd166"],
+    },
+  ];
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  pieChartType = "doughnut";
+  ngOnInit() {}
 }
