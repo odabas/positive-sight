e0a219f5a8054fb640948c19d42fc9fd72de81b4
Onur Odabas <tr.tc@msn.com>
2020-06-21 01:44:09 +0300

    Created sample data version of line chart

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { LINE_CHART_COLORS } from "../../shared/chart.colors";
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [32, 14, 46, 23, 38, 56], label: "Sentiment Analysis" },
+  { data: [12, 18, 26, 13, 28, 26], label: "Image Recognition" },
+  { data: [52, 34, 49, 53, 68, 62], label: "Forecasting" },
+];
+
+const LINE_CHART_LABELS: string[] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun"];
 
 @Component({
-  selector: 'app-line-chart',
-  templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.css']
+  selector: "app-line-chart",
+  templateUrl: "./line-chart.component.html",
+  styleUrls: ["./line-chart.component.css"],
 })
 export class LineChartComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  lineChartData = LINE_CHART_SAMPLE_DATA;
+  lineChartLabels = LINE_CHART_LABELS;
+  lineChartOptions: any = {
+    responsive: true,
+  };
 
-  ngOnInit() {
-  }
+  lineChartLegend: true;
+  lineChartType = "line";
+  lineChartColors = LINE_CHART_COLORS;
 
+  ngOnInit() {}
 }
---
+++ src/app/shared/chart.colors.ts
@@ -0,0 +1,23 @@
+export const LINE_CHART_COLORS = [
+  {
+    backgroundColor: "rgba(6,214,160,0.2)",
+    borderColor: "rgba(0,121,22,0.5)",
+    pointBackgrounColor: "#000",
+    pointHoverBackgroundColor: "#555",
+    pointBorderColor: "#555",
+  },
+  {
+    backgroundColor: "rgba(255,55,102,0.2)",
+    borderColor: "rgba(0,200,140,0.5)",
+    pointBackgrounColor: "#000",
+    pointHoverBackgroundColor: "#555",
+    pointBorderColor: "#555",
+  },
+  {
+    backgroundColor: "rgba(15,23,133,0.2)",
+    borderColor: "rgba(3,36,140,0.5)",
+    pointBackgrounColor: "#000",
+    pointHoverBackgroundColor: "#555",
+    pointBorderColor: "#111",
+  },
+];
